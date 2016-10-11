3effa01b2cd20858a2bd86fbe67e872fcb9b8ae2
Erik Pearson <eapearson@lbl.gov>
2016-10-11 10:48:59 -0700

    make the daily script configurable for file destination defaults to the normal location in /var/www/metrics

--- daily.sh
+++ daily.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# NOTE: please configure this variable to point to the destination directory
+#       for the dashboard metrics json files.
+#       By default it points to the location of the metrics export files.
+DEST=/var/www/metrics
+
 SCRIPT=$(readlink  "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
@@ -8,7 +14,7 @@ echo "Starting Dashboard Metrics analysis... "
 echo ${DATE}
 
 php php/daily.php
-# install --mode=644 data/*.json /var/www/metrics
+install -m 644 ./var/export/*.json ${DEST}
 
 echo "Finished Dashboard Metrics analysis."
 
